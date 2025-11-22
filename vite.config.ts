7f633cba44332565038b7db147e1059afbe78a51
Sayantan Biswas <sayantanbiswas10000@gmail.com>
2025-11-22 07:07:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
